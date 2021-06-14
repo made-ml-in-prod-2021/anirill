b4a6723768b82b300a6bce6e742af3c2105b27a7
Andrew Klestov <anirill@mail.ru>
2021-06-14 21:13:56 +0300

    work in progress

--- airflow_ml_dags/images/airflow-predict/predict.py
+++ airflow_ml_dags/images/airflow-predict/predict.py
@@ -14,7 +14,7 @@ def predict(input_dir: str, output_dir, models_dir):
     data = pd.read_csv(os.path.join(input_dir, "data_val.csv"))
     # target = pd.read_csv(os.path.join(input_dir, "target_val.csv"))
 
-    with open(os.path.join(models_dir, "model.pkl"), "r") as f:
+    with open(os.path.join(models_dir, "model.pkl"), "rb") as f:
         model = pickle.load(f)
 
     predictions = pd.DataFrame(model.predict(data))
--- airflow_ml_dags/images/airflow-validate/validate.py
+++ airflow_ml_dags/images/airflow-validate/validate.py
@@ -13,7 +13,7 @@ def validate(input_dir: str, models_dir: str):
     data = pd.read_csv(os.path.join(input_dir, "data_val.csv"))
     target = pd.read_csv(os.path.join(input_dir, "target_val.csv"))
 
-    with open(os.path.join(models_dir, "model.pkl"), "r") as f:
+    with open(os.path.join(models_dir, "model.pkl"), "rb") as f:
         model = pickle.load(f)
 
     score = dict()
